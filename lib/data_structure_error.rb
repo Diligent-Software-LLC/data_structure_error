6abe4a7faf69504ee4ff748dd49c672a76afb3e5
Bradley <bradtannor@gmail.com>
2019-11-11 10:54:51 -0800

    Initialized repository.

---
+++ lib/data_structure_error.rb
@@ -0,0 +1,53 @@
+require "data_structure_error/version"
+
+# DataStructureError < ArgumentTypeError.
+# Creational pattern: Abstract Factory.
+# @abstract: defines a DataStructureError exception class interface. Subclasses
+# ArgumentTypeError.
+class DataStructureError #< ArgumentTypeError
+
+  # initialize(message = nil).
+  # @abstract: the constructor.
+  # @param message: the exception explanation.
+  def initialize(message = nil)
+  end
+
+  # message().
+  # @abstract: getter method. Gets and returns the String message.
+  def message()
+  end
+
+  # try(argument_type).
+  # @abstract: in the case the argumentis a data type or an unacceptable data
+  # structure, raises. Takes an unknown type object.
+  # @param argument_type: a presumed data structure object.
+  def try(argument_type)
+  end
+
+  private
+
+  # message=(explanation).
+  # @abstract:
+  # Setter method. Sets the message attribute the argued explanation String.
+  # @param explanation: a String explaining the exception.
+  def message=(explanation)
+  end
+
+  # choose(explanation).
+  # @abstract:
+  # Chooses the appropriate explanation. In the case no explanation String was
+  # argued, the default explanation is the explanation.
+  # @param explanation: a String or NilClass object.
+  def choose(explanation)
+  end
+
+  # type?(presumed_data_structure).
+  # @abstract:
+  # Boolean method. Returns true in the case the object is an acceptable data
+  # structure. Returns false otherwise.
+  # @param presumed_data_structure: a presumed acceptable data structure
+  # object.
+  def type?(presumed_data_structure)
+  end
+
+end
