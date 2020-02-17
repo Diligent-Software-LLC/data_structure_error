d8632d49beb63f801d0c7606ac9db9769e78aab8
Bradley J. Tannor <bradleytannor@gmail.com>
2020-02-17 09:29:53 -0800

    Removed Travis CI Bundler Caching.
    Another Bundler problem. Travis CI caches the bundles, and somewhere the file loading is broken. Removed the caching configuration.

--- test/data_structure_error_test.rb
+++ test/data_structure_error_test.rb
@@ -2,7 +2,7 @@ require "test_helper"
 
 class DataStructureErrorTest < Minitest::Test
 
-def test_that_it_has_a_version_number
+  def test_that_it_has_a_version_number
     refute_nil ::DataStructureError::VERSION
   end
 
