df857d264f790ba0da3fac2c115d4ebc289e9d7a
Bradley J. Tannor <bradleytannor@gmail.com>
2020-04-01 06:57:59 -0700

    Development in Progress
    Fixed 1.2 bugs. Added GNU General Public License. Edited copyright statements. Fixed YARD. Fixed 'require' statements. Rewrote data_structure_error.rb. Rewrote data_structure_error_test.rb. Added a .yardopts file. Renamed data_structure_error.rb data_structure_error_int.rb. Renamed data_structure_error_test.rb data_structure_error_int_test.rb.

--- lib/data_structure_error/version.rb
+++ lib/data_structure_error/version.rb
@@ -1,6 +1,10 @@
 # Copyright (C) 2020 Diligent Software LLC. All rights reserved. Released
-# under the MIT License.
+# under the GNU General Public License, Version 3. Refer LICENSE.txt.
 
-class DataStructureError < TypeError
+# DataStructureErrorInt.
+# @abstract
+# @class_description
+#   Declares a DataStructureError exception class interface.
+class DataStructureErrorInt < TypeError
   VERSION = "1.2.1"
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,4 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "data_structure_error"
 
+require_relative "../lib/data_structure_error_int"
 require "minitest/autorun"
